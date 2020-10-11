72558d221829f2f2c94a20c1024bbc2ac9deb707
Danko Aleksejevs <danko@very.lv>
2020-10-11 17:16:47 +0300

    Reexport entities in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { DxfDocument } from './dxf-document';
 export { DxfWriter, DxfTextWriter } from './dxf-writer';
 export { DxfObject } from './dxf-object';
+
+import * as entities from './entities';
+export { entities };
